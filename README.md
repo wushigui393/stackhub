b67ace9ab542e9eb64d23fee7e67a6d1e5db49d8
wushigui393 <1783627061@qq.com>
2025-08-05 15:07:19 +0800

    解决打包失败问题

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+<!--
+ * @Author: wushigui393 1783627061@qq.com
+ * @Date: 2025-04-08 18:05:37
+ * @LastEditors: wushigui393 1783627061@qq.com
+ * @LastEditTime: 2025-08-05 14:50:20
+ * @FilePath: \my-next-app\README.md
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/pages/api-reference/create-next-app).
 
 ## Getting Started
@@ -12,4 +20,3 @@ yarn dev
 pnpm dev
 # or
 bun dev
-```
--- compress-images.js
+++ compress-images.js
@@ -1,17 +1,27 @@
 import imagemin from 'imagemin';
 import imageminMozjpeg from 'imagemin-mozjpeg';
 import imageminPngquant from 'imagemin-pngquant';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 (async () => {
-  const files = await imagemin(['public/images/**/*.{jpg,png}'], {
-    destination: 'public/images',
-    plugins: [
-      imageminMozjpeg(),
-      imageminPngquant({
-        quality: [0.6, 0.8]
-      })
-    ]
-  });
+  try {
+    const files = await imagemin([path.join(__dirname, 'public/images/**/*.{jpg,png}')], {
+      destination: path.join(__dirname, 'public/images'),
+      plugins: [
+        imageminMozjpeg(),
+        imageminPngquant({
+          quality: [0.6, 0.8]
+        })
+      ]
+    });
 
-  console.log('Images optimized:', files.map(file => file.path));
+    console.log('Images optimized:', files.map(file => file.path));
+  } catch (error) {
+    console.error('Image compression failed:', error);
+    process.exit(1);
+  }
 })();
