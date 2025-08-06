2ee67993576d722f1ffb1b7f78090900e2b58012
wushigui393 <1783627061@qq.com>
2025-08-06 14:51:12 +0800

    修复问题

--- next.config.mjs
+++ next.config.mjs
@@ -7,10 +7,7 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 /** @type {import('next').NextConfig} */
-import withOptimizedImages from 'next-optimized-images';
-
-const nextConfig = withOptimizedImages({
-  // 配置选项
+const nextConfig = {
   // reactStrictMode: true,
   output: 'export', // 关键配置
   images: {
@@ -22,9 +19,10 @@ const nextConfig = withOptimizedImages({
   env: {
     API_URL: 'https://api.example.com',
   },
-  optimizeImages: true,
-  optimizeImagesInDev: false,
-  publicExcludes: ['!&zwnj;**/*.jpg', '!**&zwnj;/*.png']
-});
+  images: {
+    formats: ['image/webp'],
+    deviceSizes: [640, 1080, 1600]
+  }
+};
 
 export default nextConfig;
